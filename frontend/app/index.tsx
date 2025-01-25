46713e75e5c21d5f5e7a60b82434307df1d02300
Reuben Daniel <reubendaniel41@gmail.com>
2025-01-25 15:43:56 -0600

    Started landing page

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0b0215',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: '#FFFFFF',
+  },
+});
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Napful</Text>
     </View>
   );
+
 }
