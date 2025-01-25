f1c33ec383112f42db1030ad7f32154a9088d686
Reuben Daniel <reubendaniel41@gmail.com>
2025-01-25 17:34:56 -0600

    Added a font

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Text, View, Image, Animated, ScrollView, Dimensions, StyleSheet } from "react-native";
 import { Link } from 'expo-router';
+import { useFonts } from 'expo-font';
 
 
 const styles = StyleSheet.create({
@@ -12,11 +13,13 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#FFFFFF',
+    fontFamily: 'Azeret-Mono',
   },
   title: {
     color: '#FFFFFF',
     fontSize: 40,
     paddingBottom: 60,
+    fontFamily: 'Azeret-Mono',
   },
   button: {
     borderWidth: 3,
@@ -25,17 +28,28 @@ const styles = StyleSheet.create({
     fontSize: 20,
     padding: 8,
     color: '#FFFFFF',
+    fontFamily: 'Azeret-Mono',
   }
 });
 
 export default function Index() {
+  const [loaded, error] = useFonts({
+    'Azeret-Mono': require('../assets/fonts/AzeretMono-VariableFont_wght.ttf'),
+  });
+
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.title}>Napful</Text>
       </View>
       <View>
-        <Link href="/inputs">
+        <Link href="/inputs" style={styles.button}>
           <Button title="Lights out?" />
         </Link>
       </View>
